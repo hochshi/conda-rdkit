04b9666c870117bf620e045b3c972b67780104c0
hochshi <hochshi@gmail.com>
2017-11-12 22:10:51 +0200

    Build is working time to deploy :)

--- rdkit/pkg_version.py
+++ rdkit/pkg_version.py
@@ -9,9 +9,3 @@ if rdkitVersion.endswith(".dev1"):
     pkg_version = rdkitVersion[:-1] + datetime.today().strftime("%Y%m%d")
 else:
     pkg_version = rdkitVersion
-
-version_path = os.path.join(os.environ['SRC_DIR'], '__conda_version__.txt')
-
-with open(version_path, "wt") as version_file:
-    print(pkg_version, file=version_file)
-
